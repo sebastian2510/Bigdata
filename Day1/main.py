f52677f07e13765d9449517f6427edf39a864ee5
WidovV <seba9188@elev.tec.dk>
2024-08-26 11:04:13 +0200

    Done

--- Day1/main.py
+++ Day1/main.py
@@ -1,31 +1,44 @@
 import requests
 import json
+import time
 
 class Data:
     def __init__(self, month, gridcompany, activesupplierpergridarea):
-        self.data 
         self.Month = month
         self.GridCompany = gridcompany
         self.ActiveSupplierPerGridArea = activesupplierpergridarea
+    
+    def to_dict(self):
+        return {
+            "Month": self.Month,
+            "GridCompany": self.GridCompany,
+            "ActiveSupplierPerGridArea": self.ActiveSupplierPerGridArea
+        }
 
 class API:
-    url: str = "https://api.energidataservice.dk/dataset/ElectricitySuppliersPerGridarea?offset=0&sort=Month%20DESC"
     @staticmethod
-    def extract(self):
-        response = requests.get(self.url)
-        return response.json()
+    def extract():
+        url: str = "https://api.energidataservice.dk/dataset/ElectricitySuppliersPerGridarea?offset=0&sort=Month%20DESC"
+        response = requests.get(url)
+        return response.json()["records"]
     
     @staticmethod
-    def transform(self, data):
-        transfomed_data: list[Data] = [Data(d["Month"], d["GridCompany"], d["ActiveSupplierPerGridArea"]) for d in data]
+    def transform(data):
+        transfomed_data: list[Data] = [Data((d["Month"]), d["GridCompany"], d["ActiveSupplierPerGridArea"]) for d in data]
         return transfomed_data
     
     @staticmethod
-    def load(self, data):
+    def load(data: list[Data]):
         with open("data.json", "w") as f:
-            json.dump(data, f)
+            json.dump([d.to_dict() for d in data], f, indent=4)
 
 def main():
-    data = API.extract()
-    transformed_data = API.transform(data)
-    API.load(transformed_data)
+    while True:
+        data: list[Data] = API.extract()
+        transformed_data = API.transform(data)
+        API.load(transformed_data)
+        time.sleep(60*60*24*30) # 1 month
+
+
+if __name__ == "__main__":
+    main()
