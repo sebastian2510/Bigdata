e20ccac0baae20b1d2771551d602e6e968dbfe25
WidovV <seba9188@elev.tec.dk>
2024-08-29 08:12:43 +0200

    Updates

--- Day3/word_count_spark_context_on_DFS.py
+++ Day3/word_count_spark_context_on_DFS.py
@@ -16,9 +16,27 @@ def word_count(data: str) -> dict[str, int]:
             word_counts[word] = 1
     return word_counts
 
-def save_data(data):
-    with open("output.txt", "w") as f:
-        f.write(data)
+def save_data(sc, saved_data):
+    """
+    Save the data to a file called output.txt
+    It shall be saved in /input_dir directory in the HDFS
+    """
+    path = "/input_dir/output.txt"
+
+    # Access the Hadoop FileSystem
+    hadoop_conf = sc._jsc.hadoopConfiguration()
+    hadoop_fs = sc._jvm.org.apache.hadoop.fs.FileSystem.get(hadoop_conf)
+    output_path = sc._jvm.org.apache.hadoop.fs.Path(path)
+
+    if hadoop_fs.exists(output_path):
+        hadoop_fs.delete(output_path, True)
+
+    output_stream = hadoop_fs.create(output_path)
+    try:
+        for data in saved_data:
+            output_stream.write(f"{data}\n".encode("utf-8"))
+    finally:
+        output_stream.close()
 
 def main():
     conf = SparkConf().setAppName("WordCount")
@@ -33,7 +51,7 @@ def main():
     for new_data in saved_data:
         print(new_data)
     print(f"Word count: {len(text)}")
-    save_data(saved_data)
+    save_data(sc, saved_data)
     sc.stop()
 
 if __name__ == "__main__":
--- Day3/word_count_spark_session_on_DFS.py
+++ Day3/word_count_spark_session_on_DFS.py
@@ -16,9 +16,27 @@ def word_count(data: str) -> dict[str, int]:
             word_counts[word] = 1
     return word_counts
 
-def save_data(data):
-    with open("output.txt", "w") as f:
-        f.write(data)
+def save_data(spark, saved_data):
+    """
+    Save the data to a file called output.txt
+    It shall be saved in /input_dir directory in the HDFS
+    """
+    path = "/input_dir/output.txt"
+
+    # Access the Hadoop FileSystem
+    hadoop_conf = spark._jsc.hadoopConfiguration()
+    hadoop_fs = spark._jvm.org.apache.hadoop.fs.FileSystem.get(hadoop_conf)
+    output_path = spark._jvm.org.apache.hadoop.fs.Path(path)
+
+    if hadoop_fs.exists(output_path):
+        hadoop_fs.delete(output_path, True)
+
+    output_stream = hadoop_fs.create(output_path)
+    try:
+        for data in saved_data:
+            output_stream.write(f"{data}\n".encode("utf-8"))
+    finally:
+        output_stream.close()
     
     
 
@@ -34,7 +52,7 @@ def main():
     for new_data in saved_data:
         print(new_data)
     print(f"Word count: {len(lines)}")
-    save_data(saved_data)
+    save_data(spark, saved_data)
     spark.stop()
 
 
